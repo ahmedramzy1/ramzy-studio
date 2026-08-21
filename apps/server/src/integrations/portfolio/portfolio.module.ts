f3065b5d59b21e1c340f08305a26554885c21b50
Ahmed Ramzy <ahmedramzy939@gmail.com>
2026-08-21 21:15:17 +0300

    Wire portfolio publication services

--- apps/server/src/integrations/portfolio/portfolio.module.ts
+++ apps/server/src/integrations/portfolio/portfolio.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PageModule } from '../../core/page/page.module';
 import { ShareModule } from '../../core/share/share.module';
 import { PortfolioController } from './portfolio.controller';
 
 @Module({
-  imports: [ShareModule],
+  imports: [
+    ShareModule,
+    PageModule,
+  ],
   controllers: [PortfolioController],
 })
 export class PortfolioModule {}
