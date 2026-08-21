1d1a179b0229af18f7c2fde84a0160649003e2f7
Ahmed Ramzy <ahmedramzy939@gmail.com>
2026-08-21 23:42:05 +0300

    Wire portfolio session exchange service

--- apps/server/src/integrations/portfolio/portfolio.module.ts
+++ apps/server/src/integrations/portfolio/portfolio.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TokenModule } from '../../core/auth/token.module';
 import { PageModule } from '../../core/page/page.module';
 import { ShareModule } from '../../core/share/share.module';
 import { PortfolioController } from './portfolio.controller';
+import { PortfolioSessionService } from './portfolio-session.service';
 
 @Module({
   imports: [
     ShareModule,
     PageModule,
+    TokenModule,
   ],
   controllers: [PortfolioController],
+  providers: [PortfolioSessionService],
 })
 export class PortfolioModule {}
